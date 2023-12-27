87bb3ebb10595a17c3012835f7f0d617d50ec0f2
Davidson <dehpacifico@gmail.com>
2023-12-26 22:44:14 -0300

    criando conexao com db

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hello world')
+const express = require('express')
+const app = express();
+
+const mysql = require('mysql2')
+
+const conexao = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'produtos'
+})
+
+conexao.connect((err) => {
+  if(err) new Error(err)
+  console.log('conectou com sucesso')
+})
+
+app.get('/', (req, res) => {
+  res.write('hello ')
+  res.end()
+})
+
+app.listen(8080, () => {
+  console.log('server on')
+})
